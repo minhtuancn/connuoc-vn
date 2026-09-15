c5592d66e7e39907ae250a6839158ee176e3e3c7
Minh Tuấn <vietkeynet@gmail.com>
2026-09-15 21:08:21 +0700

    feat(ingestion): export tide worker ingestion boundary

---
+++ services/tide-worker/src/index.ts
@@ -0,0 +1,6 @@
+export * from './contracts.js';
+export * from './fixture-adapter.js';
+export * from './identity.js';
+export * from './ingest.js';
+export * from './job-handler.js';
+export * from './repository.js';
